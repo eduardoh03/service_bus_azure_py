fe9d118371f21f457eb94a5bd2f070478f5db851
Eduardo <eduhenriquesrs@gmail.com>
2023-11-16 10:29:42 -0300

    Continuous consumption.

--- recv/main.py
+++ recv/main.py
@@ -10,24 +10,33 @@ load_dotenv()
 FULLY_QUALIFIED_NAMESPACE = os.getenv("FULLY_QUALIFIED_NAMESPACE")
 QUEUE_NAME = os.getenv("QUEUE_NAME")
 
-credential = DefaultAzureCredential()
+
+async def receive_messages():
+    credential = DefaultAzureCredential()
+    try:
+        # create a Service Bus client using the connection string
+        async with ServiceBusClient.from_connection_string(
+                conn_str=FULLY_QUALIFIED_NAMESPACE,
+                logging_enable=True) as service_bus_client:
+            async with service_bus_client:
+                # get the Queue Receiver object for the queue
+                receiver = service_bus_client.get_queue_receiver(queue_name=QUEUE_NAME)
+                async with receiver:
+                    received_msgs = await receiver.receive_messages(max_wait_time=5, max_message_count=20)
+                    for msg in received_msgs:
+                        print("Received: " + str(msg))
+                        # complete the message so that the message is removed from the queue
+                        await receiver.complete_message(msg)
+    except Exception as e:
+        print(e)
+    finally:
+        await credential.close()
 
 
-async def run():
-    # create a Service Bus client using the connection string
-    async with ServiceBusClient.from_connection_string(
-            conn_str=FULLY_QUALIFIED_NAMESPACE,
-            logging_enable=True) as service_bus_client:
-        async with service_bus_client:
-            # get the Queue Receiver object for the queue
-            receiver = service_bus_client.get_queue_receiver(queue_name=QUEUE_NAME)
-            async with receiver:
-                received_msgs = await receiver.receive_messages(max_wait_time=5, max_message_count=20)
-                for msg in received_msgs:
-                    print("Received: " + str(msg))
-                    # complete the message so that the message is removed from the queue
-                    await receiver.complete_message(msg)
+async def run_continuously():
+    while True:
+        await receive_messages()
 
 
 if __name__ == '__main__':
-    asyncio.run(run())
+    asyncio.run(run_continuously())
